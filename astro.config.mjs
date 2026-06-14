2da5e72ff8a8ff5f60ed6a94a327bcf3e591c7ff
Mayur Dayal <mayurdayal.engineer@gmail.com>
2026-06-14 20:49:08 +0530

    feat(project): initialize Astro Starlight documentation site

---
+++ astro.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'DCN',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/maadhav-codes',
+        },
+      ],
+      customCss: ['./src/styles/custom.css'],
+      expressiveCode: {
+        themes: ['catppuccin-frappe'],
+      },
+      sidebar: [
+        {
+          label: 'HTML',
+          items: [{ autogenerate: { directory: 'html' } }],
+        },
+        {
+          label: 'CSS',
+          items: [{ autogenerate: { directory: 'css' } }],
+        },
+        {
+          label: 'JavaScript',
+          items: [{ autogenerate: { directory: 'javascript' } }],
+        },
+      ],
+    }),
+  ],
+});
